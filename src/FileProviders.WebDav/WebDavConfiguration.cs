4fe3a4514823b9eef9d5f1ae2a88ae70647b8a5a
Patrick Krämer <patagonahn@gmail.com>
2022-04-26 21:26:35 +0200

    replace WebDAV file provider with nuget package

--- src/FileProviders.WebDav/WebDavConfiguration.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FileProviders.WebDav
-{
-    public class WebDavConfiguration
-    {
-        public string BaseUri { get; set; }
-        public string User { get; set; }
-        public string Password { get; set; }
-    }
-}
